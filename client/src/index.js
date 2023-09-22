b0c0210117e378eb2fbfac5b203bc0b7a0b55fe1
bernie-graves <berniejr01@gmail.com>
2023-09-22 16:08:51 -0700

    updated routes

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,29 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import FavesPage from "./pages/faves";
+import SharePage from "./pages/sharePage";
+import FriendsPage from "./pages/friendsPage";
+import AddFriendsPage from "./pages/addFriendPage";
+import FriendsSimilarityPage from "./pages/friendSimilarityPage";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Router>
+      <Routes>
+        {/* Define routes here */}
+        <Route path="/" Component={HomePage} />
+        <Route path="/faves" Component={FavesPage} />
+        <Route path="/friends" Component={FriendsPage} />
+        <Route path="/share" Component={SharePage} />
+        <Route path="/add_friend" Component={AddFriendsPage} />
+        <Route path="/friend_similarity" Component={FriendsSimilarityPage} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
